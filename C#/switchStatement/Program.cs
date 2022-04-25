55118ad9fd7f27a13959b75c96e69b3737060604
TylerWWhiteRevature <tyler274@revature.net>
2022-04-25 14:07:53 -0500

    switch update

--- C#/switchStatement/Program.cs
+++ C#/switchStatement/Program.cs
@@ -6,7 +6,19 @@ namespace switchStatement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int temperature = 54;
+            switch (temperature)
+            {
+                case < 32:
+                    Console.WriteLine($"The temperature is {temperature}, is it snowing?")
+                break;
+                case > 32:
+                    Console.WriteLine($"The temperature is {temperature}, still need a jacket.");
+                break;
+                default:
+                    Console.WriteLine($"The temperature is {temperature}, death.");
+                break;
+            }
         }
     }
 }
