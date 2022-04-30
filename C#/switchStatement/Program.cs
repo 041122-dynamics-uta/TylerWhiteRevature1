505d7711a33f1e1eee32ea78076c152a765d21c7
TylerWWhiteRevature <tyler274@revature.net>
2022-04-30 12:34:57 -0500

    finished switch statement

--- C#/switchStatement/Program.cs
+++ C#/switchStatement/Program.cs
@@ -9,14 +9,17 @@ namespace switchStatement
             int temperature = 54;
             switch (temperature)
             {
-                case <= 32:
-                    Console.WriteLine($"The temperature is {temperature}, is it snowing?");
+                case < 32:
+                    Console.WriteLine($"The temperature is {temperature} degrees, brrrr.");
                 break;
-                case > 32:
-                    Console.WriteLine($"The temperature is {temperature}, still need a jacket.");
+                case 32:
+                    Console.WriteLine($"The temperature is {temperature} degrees, the freezing point of water.");
+                break;
+                case < 90:
+                    Console.WriteLine($"The temerature is {temperature} degrees, a good temperature for humans.");
                 break;
                 default:
-                    Console.WriteLine($"The temperature is {temperature}, death.");
+                    Console.WriteLine($"The temperature is {temperature}, are you sweating?");
                 break;
             }
         }
